d8769a99cf10e7237c05a44649e5dd9785c6b2a5
Tim Overbay <tim.overbay@atkinsglobal.com>
2016-10-30 19:23:42 -0700

    Initial Commit

---
+++ RwMod.Console/Program.cs
@@ -0,0 +1,90 @@
+﻿using RwMod.Model;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using CONS = System.Console;
+
+namespace RwMod.Console
+{
+    public static class StringExtensions
+    {
+        public static string ToStringOrDefault(this string value, string defaultValue = "[null]")
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return defaultValue;
+            else
+                return value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathToXmlFile = @"D:\Users\Tim\SteamLibrary\steamapps\common\RimWorld\Mods\Core\Defs\WorkTypeDefs\WorkTypes.xml";
+
+            WorkTypeDefs result;
+            if (DeserializeXml(pathToXmlFile, out result))
+            {
+                WriteWorkTypeDefsToConsole(result);
+            }
+
+            CONS.ReadLine();
+        }
+
+        private static void WriteWorkTypeDefsToConsole(WorkTypeDefs result)
+        {
+            CONS.Clear();
+            CONS.WriteLine(nameof(WorkTypeDefs));
+
+            foreach (var item in result.Items)
+            {
+                WriteItemToConsole(item);
+            }
+        }
+
+        private static void WriteItemToConsole(WorkTypeDef item)
+        {
+            CONS.WriteLine($"  {nameof(WorkTypeDef)}");
+            CONS.WriteLine($"    defName: {item.Name.ToStringOrDefault()}");
+            CONS.WriteLine($"    labelShort: {item.ShortLabel.ToStringOrDefault()}");
+            CONS.WriteLine($"    pawnLabel: {item.PawnLabel.ToStringOrDefault()}");
+            CONS.WriteLine($"    gerundLabel: {item.GerundLabel.ToStringOrDefault()}");
+            CONS.WriteLine($"    description: {item.Description.ToStringOrDefault()}");
+            CONS.WriteLine($"    verb: {item.Verb.ToStringOrDefault()}");
+            CONS.WriteLine($"    naturalPriority: {item.Priority}");
+            CONS.WriteLine($"    alwaysStartActive: {item.AlwaysStartActive}");
+            CONS.WriteLine($"    requireCapableColonist: {item.RequireCapableColonist}");
+            if ((item.RelevantSkills?.Any()).GetValueOrDefault())
+            {
+                CONS.WriteLine($"    relevantSkills");
+                foreach (var li in item.RelevantSkills)
+                {
+                    CONS.WriteLine($"      li: {li.ToStringOrDefault()}");
+                }
+            }
+            if ((item.WorkTags?.Any()).GetValueOrDefault())
+            {
+                CONS.WriteLine($"    workTags");
+                foreach (var li in item.WorkTags)
+                {
+                    CONS.WriteLine($"      li: {li.ToStringOrDefault()}");
+                }
+            }
+        }
+
+        private static bool DeserializeXml<T>(string pathToXmlFile, out T result)
+        {
+            result = default(T);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            using (var reader = XmlReader.Create(pathToXmlFile, new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Document, IgnoreProcessingInstructions = true }))
+            {
+                result = (T)serializer.Deserialize(reader);
+            }
+
+            return (result != null);
+        }
+    }
+}
---
+++ RwMod.Model/WorkTypeDef.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace RwMod.Model
+{
+    [Serializable]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(Namespace = "", IsNullable = false)]
+    public class WorkTypeDefs
+    {
+        [XmlElement("WorkTypeDef", Form = XmlSchemaForm.Unqualified)]
+        public WorkTypeDef[] Items { get; set; }
+    }
+
+    [Serializable]
+    [XmlType(AnonymousType = true)]
+    public class WorkTypeDef
+    {
+        [XmlElement("defName", Form = XmlSchemaForm.Unqualified)]
+        public string Name { get; set; }
+
+        [XmlElement("labelShort", Form = XmlSchemaForm.Unqualified)]
+        public string ShortLabel { get; set; }
+
+        [XmlElement("pawnLabel", Form = XmlSchemaForm.Unqualified)]
+        public string PawnLabel { get; set; }
+
+        [XmlElement("gerundLabel", Form = XmlSchemaForm.Unqualified)]
+        public string GerundLabel { get; set; }
+
+        [XmlElement("description", Form = XmlSchemaForm.Unqualified)]
+        public string Description { get; set; }
+
+        [XmlElement("verb", Form = XmlSchemaForm.Unqualified)]
+        public string Verb { get; set; }
+
+        [XmlElement("naturalPriority", Form = XmlSchemaForm.Unqualified)]
+        public int Priority { get; set; }
+
+        [XmlElement("alwaysStartActive", Form = XmlSchemaForm.Unqualified)]
+        public bool AlwaysStartActive { get; set; }
+
+        [XmlElement("requireCapableColonist", Form = XmlSchemaForm.Unqualified)]
+        public bool RequireCapableColonist { get; set; }
+
+        [XmlArray("workTags", Form = XmlSchemaForm.Unqualified)]
+        [XmlArrayItem("li", Form = XmlSchemaForm.Unqualified)]
+        public string[] WorkTags { get; set; }
+
+        [XmlArray("relevantSkills", Form = XmlSchemaForm.Unqualified)]
+        [XmlArrayItem("li", Form = XmlSchemaForm.Unqualified)]
+        public string[] RelevantSkills { get; set; }
+    }
+}
